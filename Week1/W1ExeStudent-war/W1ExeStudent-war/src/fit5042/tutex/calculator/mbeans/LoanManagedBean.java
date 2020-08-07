dfc02e7b3a1b4b0bedc5c16e4a3a258d9f956da6
wang keyang <964074717@qq.com>
2020-08-07 14:06:01 +1000

    update the calculate

---
+++ Week1/W1ExeStudent-war/W1ExeStudent-war/src/fit5042/tutex/calculator/mbeans/LoanManagedBean.java
@@ -0,0 +1,52 @@
+package fit5042.tutex.calculator.mbeans;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+
+import fit5042.tutex.calculator.MonthlyPaymentCalculator;
+import fit5042.tutex.calculator.navigation.Navigation;
+import fit5042.tutex.repository.entities.Loan;
+
+/**
+ * @author:Keyang Wang
+ * @version:
+ * @create time：4 Aug 2020 3:37:07 pm
+ * @desc:
+ */
+// this is java bean
+@ManagedBean
+@SessionScoped
+public class LoanManagedBean implements Serializable {
+	@EJB
+	private MonthlyPaymentCalculator calculator;
+	private Loan loan;
+
+	public LoanManagedBean() {
+		this.loan = new Loan();
+	}
+
+	public MonthlyPaymentCalculator getCalculator() {
+		return calculator;
+	}
+
+	public void setCalculator(MonthlyPaymentCalculator calculator) {
+		this.calculator = calculator;
+	}
+
+	public Loan getLoan() {
+		return loan;
+	}
+
+	public void setLoan(Loan loan) {
+		this.loan = loan;
+	}
+	public String calculate() {
+		 this.loan.setMonthlyPayment(this.calculator.calculate(loan.getPrinciple(), loan.getNumberOfYears(),
+				 loan.getInterestRate()));
+				  return Navigation.index.toString();
+	}
+
+}
